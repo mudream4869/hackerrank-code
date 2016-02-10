62a90e9bc9d4b83066aab2ef011ec92c84de4695
Mudream <mudream4869@gmail.com>
2016-02-10 11:00:03 +0800

    add corr 1~3

---
+++ Artificial Intelligence/Correlation and Regression Lines - A Quick Recap #1/main.r
@@ -0,0 +1,13 @@
+# Here are the test scores of 10 students in physics and history:
+
+# Physics Scores  15  12  8   8   7   7   7   6   5   3
+# History Scores  10  25  17  11  13  17  20  13  9   15
+# Compute Karl Pearson’s coefficient of correlation between these scores. 
+# Compute the answer correct to three decimal places.
+
+phys <- c(15, 12, 8, 8, 7, 7, 7, 6, 5, 3)
+hist <- c(10, 25, 17, 11, 13, 17, 20, 13, 9, 15)
+
+cat(round(
+    cor(phys, hist)
+, digits = 3), "\n")
